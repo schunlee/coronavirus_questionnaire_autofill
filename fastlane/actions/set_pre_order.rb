ef6857bfac8b6a0db36dfae0ea0541cf1af58344
schunlee <bill.lee.1018@163.com>
2020-12-10 10:25:46 +0800

    Update set_pre_order.rb

--- fastlane/actions/set_pre_order.rb
+++ fastlane/actions/set_pre_order.rb
@@ -25,7 +25,8 @@ module Fastlane
         app_id = app.apple_id
         app1 = Spaceship::ConnectAPI.get_app(app_id: app_id).first
         puts "^^^^^^^^^^^^^^^^^^^^"
-        puts app1.appStoreVersions
+        territory_ids = Spaceship::ConnectAPI::Territory.all.map(&:id)
+        puts territory_ids
         puts "^^^^^^^^^^^^^^^^^^^^"
         puts app1.update()
         availability = app.availability
