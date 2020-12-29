72243feec4bea3431edf72b18d0864a2f2908ecf
schunlee <bill.lee.1018@163.com>
2020-12-29 11:47:37 +0800

    Add files via upload

---
+++ fastlane/actions/upload_video.rb
@@ -0,0 +1,121 @@
+module Fastlane
+  module Actions
+    module SharedValues
+      UPLOAD_VIDEO_CUSTOM_VALUE = :UPLOAD_VIDEO_CUSTOM_VALUE
+    end
+
+    class UploadVideoAction < Action
+      def self.run(params)
+        # fastlane will take care of reading in the parameter and fetching the environment variable:
+        UI.message "💖💗💖💗 BEGIN 💖💗💖💗"
+        video_path = params[:video_path]
+        language = params[:language]
+
+        UI.message("find video_path:#{video_path} 🌸")
+        UI.message("find language:#{language} 🌸")
+
+        Spaceship::Tunes.login($FASTLANE_USER, $FASTLANE_PASSWORD)
+        app = Spaceship::ConnectAPI::App.find(ENV['APP_IDENTIFIER'])
+
+        app_version = app.get_edit_app_store_version(platform:Spaceship::ConnectAPI::Platform::IOS)
+        localizations = app_version.get_app_store_version_localizations
+        if language:
+            puts "Only update video app preview on #{language}"
+            localization.each do |localization|
+                lan = localization.locale
+                if lan == language
+                    upload_video(localization, lan, video_path)
+                end #localization
+        else:
+             localization.each do |localization|
+                 lan = localization.locale
+                 upload_video(localization, lan, video_path)
+        end #language
+
+      end
+
+      def self.upload_video(localization, lan, video_path)
+          preview_sets = localization.get_app_preview_sets
+          Spaceship::ConnectAPI::AppPreviewSet::PreviewType::ALL.each do |preview_type|
+              puts("Process preview type #{preview_type}")
+
+              # find the preview set for this type
+              preview_set = preview_sets.find do |set|
+                  set.preview_type == preview_type
+              end
+              if preview_set.nil?
+                  puts("Skipping #{preview_type}; not create first in app store")
+                  next
+              end
+              # delete all existing previews
+              if !preview_set.app_previews.empty?
+                  preview_set.app_previews.each do |app_preview|
+                      puts("Deleting #{app_preview.id}")
+                      app_preview.delete!
+                  end
+              end
+              puts preview_set.upload_preview(path: video_path, wait_for_processing: false)
+              puts "video #{video_path} be uploaded on App Store (language >> #{lan})"
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.description
+        "A short description with <= 80 characters of what this action does"
+      end
+
+      def self.details
+        # Optional:
+        # this is your chance to provide a more detailed description of this action
+        "You can use this action to do cool things..."
+      end
+
+      def self.available_options
+        # Define all options your action supports.
+
+        # Below a few examples
+        [
+          FastlaneCore::ConfigItem.new(key: :video_path,
+                                       env_name: "FL_UPLOAD_VIDEO_API_TOKEN", # The name of the environment variable
+                                       description: "API Token for UploadVideoAction")
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :language,
+                                       env_name: "FL_UPLOAD_VIDEO_DEVELOPMENT",
+                                       description: "Create a development certificate instead of a distribution one")
+        ]
+      end
+
+      def self.output
+        # Define the shared values you are going to provide
+        # Example
+        [
+          ['UPLOAD_VIDEO_CUSTOM_VALUE', 'A description of what this value contains']
+        ]
+      end
+
+      def self.return_value
+        # If your method provides a return value, you can describe here what it does
+      end
+
+      def self.authors
+        # So no one will ever forget your contribution to fastlane :) You are awesome btw!
+        ["Your GitHub/Twitter Name"]
+      end
+
+      def self.is_supported?(platform)
+        # you can do things like
+        #
+        #  true
+        #
+        #  platform == :ios
+        #
+        #  [:ios, :mac].include?(platform)
+        #
+
+        platform == :ios
+      end
+    end
+  end
+end
