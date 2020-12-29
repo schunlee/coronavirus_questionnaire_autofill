0de5c3ffedde4b4af9fa1aa5e34adfcffeae80e5
schunlee <bill.lee.1018@163.com>
2020-12-29 15:30:51 +0800

    Update upload_video.rb

--- fastlane/actions/upload_video.rb
+++ fastlane/actions/upload_video.rb
@@ -44,7 +44,7 @@ module Fastlane
       def self.upload_video(localization, lan, size, video_path, size_preview_dict)
           preview_sets = localization.get_app_preview_sets
           #Spaceship::ConnectAPI::AppPreviewSet::PreviewType::ALL.each do |preview_type|
-          preview_types = size_preview_dict[size]
+          preview_types = size_preview_dict[size.to_s]
           puts size_preview_dict
           puts size
           puts preview_types
