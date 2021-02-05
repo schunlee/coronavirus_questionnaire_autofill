5bd069226e30b77d33b61998cfcb66b2cc749075
schunlee <bill.lee.1018@163.com>
2021-02-05 17:06:29 +0800

    Update positions.js

--- positions.js
+++ positions.js
@@ -11,7 +11,7 @@ function slip(height,height2, duration){
     swipe(device.width/w1,height_1,device.width/w2,height_2,duration);
     //提示等待时间。
     console.info("sleep ... " + time_random)
-    // sleep(time_random);
+    sleep(time_random);
 }
 
 function CurentTime(){ 
@@ -50,7 +50,6 @@ function close_app(packageName) {
     let is_sure = textMatches(/(.*强.*|.*行.*|.*停.*|.*止.*)/).findOne();
 
     if (is_sure.enabled()) {
-        console.info("hhhhhhhh")
         sleep(3000);
         if(id("right_button").exists()){
             id("right_button").click();
@@ -63,11 +62,14 @@ function close_app(packageName) {
         if(className("android.widget.Button").depth(1).indexInParent(3).exists()){
             className("android.widget.Button").depth(1).indexInParent(3).findOne().click();
         }
-        log(app.getAppName(name) + "应用已被关闭");
-        sleep(1000);
+        console.info(app.getAppName(name) + "应用已被关闭");
+        toast(app.getAppName(name) + "应用已被关闭");
+        var sleep_time = 1000 + random(1,3)*1000;
+        sleep(sleep_time);
+        toast("sleep " + sleep_time);
         back();
     } else {
-        log(app.getAppName(name) + "应用不能被正常关闭或不在后台运行");
+        console.info(app.getAppName(name) + "应用不能被正常关闭或不在后台运行");
         back();
     }
 }
@@ -80,41 +82,58 @@ function close_app(packageName) {
 auto.waitFor();
 var appName = "BOSS直聘";
 launchApp(appName);
-sleep(4000);
+var sleep_time = 4000 + random(1,3)*1000;
+sleep(sleep_time);
+toast("sleep " + sleep_time);
 toast("Launch app - " + appName);
 
 var report_time = CurentTime();
 
 var home = id("cl_tab_4").findOnce();
+
+toast("home_button clickable" + home.clickable());
+console.info("home_button clickable" + home.clickable());
 if(home && home.clickable()){
     home.click();
-    sleep(4000);
+    var sleep_time = 4000 + random(1,3)*1000;
+    sleep(sleep_time);
+    toast("sleep " + sleep_time);
     id("rl_boss_base_info").findOne().click();
-    sleep(2000);
-    var boss_name = id("tv_name").findOne().text()
-    var company = id("tv_brand").findOne().text()
-    console.info(boss_name);
-    console.info(company);
+    var sleep_time = 2000 + random(1,3)*1000;
+    sleep(sleep_time);
+    toast("sleep " + sleep_time);
+    var boss_name = id("tv_name").findOne().text();
+    var company = id("tv_brand").findOne().text();
+    console.info("boss_name " + boss_name);
+    toast("boss_name " + boss_name);
+    console.info("boss_company " + company);
+    toast("boss_company " + company);
     id("iv_back").findOne().click();
-    sleep(4000);
+    var sleep_time = 4000 + random(1,3)*1000;
+    sleep(sleep_time);
+    toast("sleep " + sleep_time);
 
-    console.info("other")
     if(className("android.widget.ScrollView").depth(1).indexInParent(5).exists()){
-        console.info("1111111")
+        console.info("UI => 2")
         var job_count = className("android.widget.ScrollView").depth(1).indexInParent(5).findOne().find(className("android.widget.TextView"))[0].text();
-        console.info(job_count);
+        console.info("开放职位数: " + job_count);
+        toast("开放职位数: " + job_count);
         className("android.widget.ScrollView").depth(1).indexInParent(5).findOne().find(className("android.widget.TextView"))[0].parent().click();
     }else{
-        console.info("2222222222")
+        console.info("UI => 1")
         var job_count = className("android.widget.LinearLayout").depth(13).indexInParent(0).find()[1].findOne(id("tvPositionCount")).text();
-        console.info(job_count);
+        console.info("开放职位数: " + job_count);
+        toast("开放职位数: " + job_count);
         className("android.widget.LinearLayout").depth(13).indexInParent(0).find()[1].children()[0].click();
     }
-
-    sleep(5000);
-    console.info("**************8***")
+    var sleep_time = 5000 + random(1,3)*1000;
+    sleep(sleep_time);
+    toast("sleep " + sleep_time);
+    console.info("*****************")
     list_jobs(job_count)
-
+}else{
+    toast("`我的` tag没找到耶，请关闭app重来盘");
+    console.info("`我的` tag没找到耶，请关闭app重来盘");
 }
 var sleep_time = 3000 + random(1,4)*1000;
 sleep(sleep_time);
@@ -126,11 +145,15 @@ close_app(appName);
 
 
 function list_jobs(job_count){
+    console.info("开始爬取职位信息...");
+    toast("开始爬取职位信息...");
     var jobs_list = [];
     var job_status = "";
     var crawl_index = 0;
     while(true){
-        sleep(3000)
+        var sleep_time = 3000 + random(1,3)*1000;
+        sleep(sleep_time);
+        toast("sleep " + sleep_time);
         if(className("android.view.ViewGroup").depth(4).exists()){
             var jobs = className("android.view.ViewGroup").depth(4).find();
         }else{
@@ -138,13 +161,13 @@ function list_jobs(job_count){
         }
         
         console.info("jobs length => " + jobs.length);
-
-        console.info("---------------");
+        toast("jobs length => " + jobs.length);
 
         jobs.forEach(function(job_item, item_index){
-            sleep(4000);
-            console.info("order " + job_item.drawingOrder());
-            var job_order = job_item.drawingOrder();
+            var sleep_time = 4000 + random(1,3)*1000;
+            sleep(sleep_time);
+            toast("sleep " + sleep_time);
+
             // console.info(job_item.find(id("tv_job_name").indexInParent(item_index))[0].text())
             if(job_item.find(id("tv_job_name")).length){
                 var job_name_obj = job_item.find(id("tv_job_name"))[0];
@@ -153,11 +176,10 @@ function list_jobs(job_count){
                 var job_name_obj = job_item.find(id("tv_job_name"));
             }
 
-            // if(job_item.findOne(id("tv_job_name"))){
             console.info("type" + typeof(job_name_obj));
             try{
                 var job_name = job_name_obj.text();
-                toast(job_name);
+                toast("job_name " + job_name);
                 console.info(jobs_list);
                 
                 if(id("tv_job_state").exists()){
@@ -166,7 +188,9 @@ function list_jobs(job_count){
 
                 console.info("job_item_clickable: " + job_item.clickable());
                 job_item.click();
-                sleep(3000);
+                var sleep_time = 3000 + random(1,3)*1000;
+                sleep(sleep_time);
+                toast("sleep " + sleep_time);
                 payload = browse_job();
                 payload.report_time = report_time;
                 payload.boss_name = boss_name;
@@ -179,6 +203,8 @@ function list_jobs(job_count){
                     let res = http.postJson(url, JSON.stringify(payload));
                     let html_content = res.body.string();  //取页面html源码
                     let json_content = JSON.parse(html_content);
+                    console.info("Upload data to aliyun");
+                    toast("Upload data to aliyun");
                     crawl_index = crawl_index + 1;
                     jobs_list.push(job_name + payload.contacted_count + payload.viewed_count + payload.like_count + payload.shared_count + payload.job_type);
                 } 
@@ -193,15 +219,20 @@ function list_jobs(job_count){
             
             }
         );
-        sleep(3000);
+        var sleep_time = 3000 + random(1,3)*1000;
+        sleep(sleep_time);
+        toast("sleep " + sleep_time);
         console.info("-----------下拉----------");
+        toast("职位下拉 ...")
         slip(1000, 970, 30);
-        sleep(5000);
-        // if(jobs_list.length == job_count){
-        //     break
-        // }
+        var sleep_time = 5000 + random(1,3)*1000;
+        sleep(sleep_time);
+        toast("sleep " + sleep_time);
+
         if(job_status){
+            toast("退出循环");
             break;
+   
         }
     }
 }
@@ -213,11 +244,11 @@ function browse_job(){
     var flag = "";
     while(flag == ""){
         console.info("---------------------------------")
+        toast("职位具体信息爬取 ...");
         var internal_job_name = id("tv_job_name").findOne().text();
         var job_desp = id("tv_description").findOne().text();
         var job_type = id("tv_banner_title").findOne().text();
 
-        
         if(className("android.widget.LinearLayout").depth(3).find()[1]){
             var base = className("android.widget.LinearLayout").depth(3).find()[1];
             var keywords = base.find(className("android.widget.FrameLayout").depth(4));
@@ -231,15 +262,27 @@ function browse_job(){
             keywords_list.push(item.findOne(className("android.widget.TextView")).text());
         });
         slip(1000, 0, 30); //下滑
-        console.info("hello");
+        toast("下拉 ...");
+
         flag = id("tv_job_related_info_title").findOne().text();
-        console.info("hi");
+
         var contacted_count = id("tv_job_contacted_count").findOne().text();
+        console.info("沟通过 " + contacted_count);
+        toast("沟通过 " + contacted_count);
         var viewed_count = id("tv_job_viewed_count").findOne().text();
+        console.info("查看过 " + viewed_count);
+        toast("查看过 " + viewed_count);
         var like_count = id("tv_job_like_count").findOne().text();
+        console.info("感兴趣 " + like_count);
+        toast("感兴趣 " + like_count);
         var shared_count = id("tv_job_shared_count").findOne().text();
-        console.info(shared_count);
+        console.info("分享查看 " + shared_count);
+        toast("分享查看 " + shared_count);
+
         id("iv_back").findOne().click();
+        toast("返回职位列表");
+        console.info("返回职位列表");
+
         return {"internal_job_name": internal_job_name,
                 "job_desp": job_desp,
                 "keywords": keywords_list.join(","),
